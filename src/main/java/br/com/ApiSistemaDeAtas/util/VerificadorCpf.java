cf89529059211aad4c9e8a82f1ec730e889be161
Diego-k2 <degosantosiva@gmail.com>
2022-08-25 15:08:49 -0300

    add verificador de cpf

---
+++ src/main/java/br/com/ApiSistemaDeAtas/util/VerificadorCpf.java
@@ -0,0 +1,64 @@
+package br.com.ApiSistemaDeAtas.util;
+
+public class VerificadorCpf {
+
+    public static boolean isCpfValid(String cpf){
+
+        if(cpf.length() == 11){
+            if(cpf.equals(verificaDigitos(cpf))){
+                return true;
+            }
+            return false;
+        }
+
+        return false;
+    }
+
+    private static String verificaDigitos(String cpf){
+
+        int j = 10;
+        int soma = 0;
+        String cpfVerif = "";
+
+        for(int i = 0; i < 9; i++){
+            cpfVerif += cpf.charAt(i);
+            Integer n = Integer.parseInt(String.valueOf(cpf.charAt(i)));
+            soma += n * j;
+            j--;
+        }
+
+        int restoDivisao = soma % 11;
+        Integer verifPrimeiroDigito = 11 - restoDivisao;
+
+        if(verifPrimeiroDigito >= 10){
+            return cpfVerif + 0;
+        }
+
+        return verificaSegundogito(cpfVerif + verifPrimeiroDigito);
+    }
+
+    private static String verificaSegundogito(String cpf){
+
+
+        int j = 11;
+        int soma = 0;
+        String cpfVerif = "";
+
+        for(int i = 0; i < 10; i++){
+            cpfVerif += cpf.charAt(i);
+            Integer n = Integer.parseInt(String.valueOf(cpf.charAt(i)));
+            soma += n * j;
+            j--;
+        }
+
+        int restoDivisao = soma % 11;
+        Integer verifSegundoDigito = 11 - restoDivisao;
+
+        if(verifSegundoDigito >= 10){
+            return cpfVerif + 0;
+        }
+
+        return cpfVerif + verifSegundoDigito.toString();
+    }
+
+}
